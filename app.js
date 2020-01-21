8827ab755f3f9c9f44b22940e8f6cb7ffbfb1562
Wenchin <wenchin.ch@gmail.com>
2020-01-21 11:31:59 +0800

    Added socket in match page

--- app.js
+++ app.js
@@ -13,8 +13,32 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+// match page
+app.get('/match', function(req, res){
+  res.sendFile(__dirname + '/public/match.html');
+});
 
-app.listen(3000, function () {
+// socket io settings for live demo
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+  console.log('Socket: a user connected');
+  socket.on('disconnect', () => {
+    console.log('Socket: user disconnected');
+  });
+});
+
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+    console.log(msg);
+  });
+});
+
+
+http.listen(3000, function () {
   console.log('App running on port 3000!');
 });
 
