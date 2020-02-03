c145c8c3ff1b51de85c9765ed358e0a2c30b61c3
Wenchin <wenchin.ch@gmail.com>
2020-02-03 16:21:05 +0800

    Added seconds to timer

--- public/js/match.js
+++ public/js/match.js
@@ -13,17 +13,50 @@ socket.on('connect', () => {
   socket.emit('join', userName);
 });
 
+// too many people in a match: reject and redirect
+socket.on('rejectUser', (msg) => {
+  window.alert(msg);
+  window.location = '/';
+})
+
 // 拿到 questionData 顯示在前端 (once: 只有第一次拿到做，之後不動作)
 socket.once('questionData', questionObject => {
   document.getElementById('matchQuestion').innerHTML = questionObject.question;
   document.getElementById('question').innerHTML = questionObject.description;
   codemirrorEditor.setValue(questionObject.code);
+});
+
+socket.on('waitForOpponent', msg => {
+  // window.alert(msg);
+  document.getElementById('runCodeResult').innerHTML = msg;
+  // let node = document.createElement('p');
+  // node.prepend(document.createTextNode(msg);
+  // document.getElementById('terminalResult').innerHTML = node;
 })
+
+// socket.on('inOutMessage', (msgObject) => {
+//   // 自己加入：自己 terminal 顯示等待對手訊息
+//   if(msgObject.user === userName) {
+//     // let node = document.createElement('p');
+//     // node.appendChild(document.createTextNode('Hold on. We are waiting for your opponent to join...'))
+//     document.getElementById('runCodeResult').innerHTML = 'Hold on. We are still waiting for your opponent to join...';
+//     return;
+//   }
+//   // 對手加入：自己 terminal 放入兩個 div 讓用戶開始寫
+//   document.getElementById('opponentRunCodeResult').innerHTML = msgObject.message;
+//   node = document.getElementById('runCodeResult');
+//   let runCodeOutput = document.createElement('div');
+//   runCodeOutput.id = runCodeOutput;
+//   let runCodeExpected = document.createElement('div');
+//   runCodeExpected.id = runCodeExpected;
+//   node.appendChild(runCodeOutput);
+//   node.appendChild(runCodeExpected);
+// })
 
 
 // 接收 codeResult 並顯示（每次都蓋掉上次的）
 socket.on('codeResult', (resultObj) =>{
-  console.log(resultObj)
+  console.log(resultObj);
   // 顯示自己的結果在自己的 terminal
   if(resultObj.user === userName) {
     document.getElementById('runCodeOutput').innerHTML = resultObj.output;
@@ -41,7 +74,7 @@ function runCode() {
   document.getElementById("testcase").style.display = "none";
   document.getElementById("testcaseBtn").style.background = "#222222";
   // 顯示 run code
-  document.getElementById("runCodeResult").style.display = "block";
+  document.getElementById("runCodeResult").style.display = "flex";
   document.getElementById("runcodeBtn").style.background = "#555555";
 
   // send code & test to server
@@ -62,7 +95,7 @@ function runCode() {
 
 function showTestCase() {
   // 顯示 test case
-  document.getElementById("testcase").style.display = "block";
+  document.getElementById("testcase").style.display = "flex";
   document.getElementById("testcaseBtn").style.background = "#555555";
   // 隱藏 run code
   document.getElementById("runCodeResult").style.display = "none";
