f5a95531ac1f063cb836285749b11c902d8e4e2a
Wenchin <wenchin.ch@gmail.com>
2020-02-16 16:58:26 +0800

    Built member signin signup ui

--- public/js/signin.js
+++ public/js/signin.js
@@ -4,3 +4,34 @@ function tempSignin() {
   window.location.pathname= 'match_setup';
   localStorage.setItem('name', userName);
 }
+
+const switchToSignup = () => {
+  let signup = document.getElementById('signup');
+  let signin = document.getElementById('signin');
+  let button = document.getElementById('toggleButton');
+  signin.style.left = '-400px';
+  signup.style.left = '50px';
+  button.style.left = '110px'
+}
+
+const switchToSignin = () => {
+  let signup = document.getElementById('signup');
+  let signin = document.getElementById('signin');
+  let button = document.getElementById('toggleButton');
+  signin.style.left = '50px';
+  signup.style.left = '450px';
+  button.style.left = '0px'
+}
+
+const useTestUser = () => {
+  let checkbox = document.getElementById('testCheckbox');
+  let email = document.getElementById('signinEmail');
+  let password = document.getElementById('signinPassword');
+  if (checkbox.checked) {
+    email.value = 'test@test.com';
+    password.value = '123456';
+    return;
+  }
+  email.value = '';
+  password.value = '';
+}
